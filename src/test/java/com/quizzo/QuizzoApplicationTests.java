693169d4519e616b72eb35d566fb69e0414b9074
Robert <robertkania@spoko.pl>
2025-04-07 14:09:43 +0200

    Init commit

---
+++ src/test/java/com/quizzo/QuizzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
