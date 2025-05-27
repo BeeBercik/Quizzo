c162cc84627acfa73cb53467a14ac8ce3b07b3fd
Robert <robertkania@spoko.pl>
2025-05-27 18:03:58 +0200

    Add method in quizService pretending delivery of test details

--- src/main/resources/static/js/services/quizService.js
+++ src/main/resources/static/js/services/quizService.js
@@ -1,11 +1,44 @@
 
 
-export function fetchQuizInfo() {
-//     ...
+export function fetchQuizInfo(code) {
+    // fetch for qiz info
     return {
         name: "Quiz about the nature of human",
         time: 30,
         questions: 10,
         elimination: true
+    };
+
+//     return null;
+}
+
+export function getTestDetails(code) {
+    //  fetch for test details
+    return {
+        time: 30,
+        eliminations: true,
+        eliminationsCount: 3,
+        questions: [
+            {
+                question: "What color is crocodile?",
+                answers: {
+                    A: "red",
+                    B: "blue",
+                    C: "green",
+                    D: "red"
+                },
+                correctAnswer: "C"
+            },
+            {
+                question: "What is elephant?",
+                answers: {
+                    A: "animal",
+                    B: "human",
+                    C: "fish",
+                    D: "insect"
+                },
+                correctAnswer: "A"
+            }
+        ]
     };
 }
