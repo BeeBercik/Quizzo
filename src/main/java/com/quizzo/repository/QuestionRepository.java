76b36bd422598cd014253f3673771af4c30cae7e
Robert <robertkania@spoko.pl>
2025-07-10 22:48:07 +0200

    add repositories for answer, question and quiz entities

---
+++ src/main/java/com/quizzo/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.quizzo.repository;
+
+import com.quizzo.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
---
+++ src/main/java/com/quizzo/repository/QuizRepository.java
@@ -0,0 +1,9 @@
+package com.quizzo.repository;
+
+import com.quizzo.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+}
