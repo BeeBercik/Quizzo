674298cb1fbe9678b4eed3a3c7a1667d0ee54783
Robert <robertkania@spoko.pl>
2026-04-18 15:17:19 +0200

    add option elimination logic to prevent redundant checks and enforce limits

--- src/main/resources/static/js/ui/optionEliminator.js
+++ src/main/resources/static/js/ui/optionEliminator.js
@@ -4,20 +4,30 @@ import {generateError} from "./globalMessageBar.js";
 export default async function eliminateOption(testDetails, options, eliminated) {
     let random;
     let answerId;
+    const checkedOptions = [];
 
     while (true) {
         do {
             random = Math.floor(Math.random() * options.length);
-        } while (eliminated.includes(random));
+        } while (eliminated.includes(random) || checkedOptions.includes(random));
+
+        checkedOptions.push(random);
 
         answerId = options[random].dataset.value;
         const result = await getAnswerCorrectness(answerId);
 
-        if (result === null)
+        if (result === null) {
             generateError('Error during option elimination');
+            return;
+        }
 
         if (result === false)
             break;
+
+        if ((checkedOptions.length + eliminated.length) >= options.length) {
+            generateError("Cannot eliminate more bad answers");
+            return;
+        }
     }
 
     eliminated.push(random);
